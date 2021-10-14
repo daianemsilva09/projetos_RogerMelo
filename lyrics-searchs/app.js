42d9b0ac6f5c2bc044dab2ab83fb39b3a2992f97
Daiane Silva <daianemsilva09@gmail.com>
2021-10-14 17:58:22 -0300

    Começando projeto

---
+++ lyrics-searchs/app.js
@@ -0,0 +1,21 @@
+const form = document.querySelector('#form')
+const searchInput = document.querySelector('#search')
+const songsContainer = document.querySelector('#songs-container')
+const prevAndNextContainer = document.querySelector('#prev-and-next-container')
+
+const apiURL = `https://api.lyrics.ovh`
+
+form.addEventListener('submit', event =>{
+    //não disparar evento
+    event.preventDefault()
+
+    //pegar os dados inseridos na pagina / trim(pega variavel do inicio e fim e tira)
+    const searchTerm = searchInput.value.trim() 
+    //9:10 teste  
+    if (!searchInput){
+        
+    }
+
+    console.log(searchTerm)
+
+})
