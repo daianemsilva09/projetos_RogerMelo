21e718daf5a7f0520a33e26c08bd9585604af675
Daiane Silva <daianemsilva09@gmail.com>
2021-10-18 12:55:53 -0300

    finalizar depois

--- lyrics-searchs/app.js
+++ lyrics-searchs/app.js
@@ -1,9 +1,18 @@
+//finalizar depois
 const form = document.querySelector('#form')
 const searchInput = document.querySelector('#search')
 const songsContainer = document.querySelector('#songs-container')
 const prevAndNextContainer = document.querySelector('#prev-and-next-container')
 
 const apiURL = `https://api.lyrics.ovh`
+
+const fetchSongs = term => {
+    fetch(`${apiURL}/suggest/${term}`)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+    })
+}
 
 form.addEventListener('submit', event =>{
     //não disparar evento
@@ -13,8 +22,14 @@ form.addEventListener('submit', event =>{
     const searchTerm = searchInput.value.trim() 
     //9:10 teste  
     if (!searchTerm){
+        //inserindo a Li no Html
        songsContainer.innerHTML = `<li class="warning-message">Por favor, digite um termo válido </li>`
+       return
     }
+    //Chamando a função pra procurar
+    fetchSongs(searchTerm)
+
+
 
     
 
